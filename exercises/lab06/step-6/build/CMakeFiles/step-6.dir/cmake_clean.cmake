ec1343cdcac048c8920efa7575d405248a385687
cpacilio <cpacilio@sissa.it>
2019-03-03 14:57:49 +0000

    report

---
+++ exercises/lab06/step-6/build/CMakeFiles/step-6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-6.dir/step-6.cc.o"
+  "step-6.pdb"
+  "step-6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exercises/report.md
@@ -0,0 +1,3 @@
+# Deal.II Course: Report by Costantino Pacilio
+## Step 1
+File link: [step1](lab01/step-1/step-1_mine.cc)
